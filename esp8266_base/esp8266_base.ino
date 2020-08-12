699c471a645687ead5cab6485d90b5f20245f141
EliForester <eli.j.forester@gmail.com>
2020-08-12 17:43:30 +0900

    Updates for http put

--- esp8266_base/esp8266_base.ino
+++ esp8266_base/esp8266_base.ino
@@ -1,11 +1,14 @@
 #include <DHTesp.h>
 #include <ESP8266WiFi.h>
-
-DHTesp dht;
+#include <ESP8266HTTPClient.h>
 
 const int dhtPin = 14;
 const char* ssid = "" // Add network ID
 const char* password = "" // Add password
+const char* http_server = "http://" // Add server IP address
+
+DHTesp dht;
+HTTPClient http;
 
 void setup(){
   Serial.begin(115200);
@@ -32,11 +35,25 @@ void loop(){
   float temperature = dht.getTemperature();
   float humidity = dht.getHumidity();
 
-  Serial.print("Status = ");
-  Serial.println(dht.getStatusString());
-  Serial.print("Temperature = ");
-  Serial.println(temperature, 1);
-  Serial.print("Humidity = ");
-  Serial.println(humidity, 1);
-  delay(2000);
+  if(WiFi.status()== WL_CONNECTED){
+    http.begin(http_server);
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.PUT("Putting data");
+    if(httpResponseCode>0){
+      // handle server response
+    }
+  
+    String response = http.getString();
+ 
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+
+    http.end();
+  }
+
+  Serial.print("Status = "); Serial.print(dht.getStatusString());
+  Serial.print("\t Temperature = "); Serial.print(temperature, 1);
+  Serial.print("\t Humidity = "); Serial.println(humidity, 1);
+  delay(60 * 1000);
 }
+
